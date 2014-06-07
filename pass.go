3ebefdcd28fc6cc4b31645d1577cd71130225810
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2014-06-06 19:29:16 -0500

    Use bitly/go-simplejson, do real error handling

--- pass.go
+++ pass.go
@@ -2,64 +2,58 @@ package main
 
 import (
   "bytes"
-  "compress/gzip"
   "compress/flate"
-  "io/ioutil"
+  "compress/gzip"
   "crypto/aes"
-  "crypto/rand"
   "crypto/cipher"
-  "encoding/json"
+  "crypto/rand"
+  "errors"
   "fmt"
+  "io/ioutil"
+  js "github.com/bitly/go-simplejson"
   "code.google.com/p/go.crypto/scrypt"
 )
+
+// NOTES:
+//  - need to encrypt PII separately from the main database
+//  - decrypt once to get all non-PII with encrypted PII, then decrypt each
+//    individual PII (passwords, custom fields, etc.) as needed. this should
+//    prevent more than one PII from existing in memory at a time.
 
 // the size of the salt data that's pre-pended to the encrypted data
 const SaltSize = 32
 
-func dumpJSON(data *map[string]interface{}) []byte {
-  result, err := json.Marshal(data)
-  if err != nil { panic(err) }
-  return result
-}
-
-func loadJSON(data []byte) map[string]interface{} {
-  result := make(map[string]interface{})
-  err := json.Unmarshal(data, &result)
-  if err != nil { panic(err) }
-  return result
-}
-
 // gzip some data
-func compress(data []byte) []byte {
+func compress(data []byte) ([]byte, error) {
   var result bytes.Buffer
   writer, err := gzip.NewWriterLevel(&result, flate.BestCompression)
 
-  if err != nil { panic(err) }
+  if err != nil { return nil, err }
 
   // compress our data
   writer.Write(data)
   writer.Close()
 
-  return result.Bytes()
+  return result.Bytes(), nil
 }
 
 // gunzip some data
-func decompress(data []byte) []byte {
+func decompress(data []byte) ([]byte, error) {
   b := bytes.NewBuffer(data)
   reader, err := gzip.NewReader(b)
 
-  if err != nil { panic(err) }
+  if err != nil { return nil, err }
 
   // compress our data and close the reader
   result, err := ioutil.ReadAll(reader)
-  if err != nil { panic(err) }
+  if err != nil { return nil, err }
   reader.Close()
 
-  return result
+  return result, nil
 }
 
 // encrypt some data using the given password
-func encrypt(plaintext []byte, password string) []byte {
+func encrypt(plaintext []byte, password string) ([]byte, error) {
   // overall output is the salt, followed by the IV, followed by the ciphertext
   output := make([]byte, SaltSize + aes.BlockSize + len(plaintext))
 
@@ -69,27 +63,27 @@ func encrypt(plaintext []byte, password string) []byte {
   ciphertext := output[SaltSize + aes.BlockSize:]
 
   // randomize the salt and the IV
-  if _, err := rand.Read(salt); err != nil { panic(err) }
-  if _, err := rand.Read(iv); err != nil { panic(err) }
+  if _, err := rand.Read(salt); err != nil { return nil, err }
+  if _, err := rand.Read(iv); err != nil { return nil, err }
 
   // hash the password into an AES-256 (32-byte) key using the generated salt
   key := hashPassword(password, salt)
 
   // encrypt the plaintext
   block, err := aes.NewCipher(key)
-  if err != nil { panic(err) }
+  if err != nil { return nil, err }
 
   stream := cipher.NewCFBEncrypter(block, iv)
   stream.XORKeyStream(ciphertext, plaintext)
 
-  return output
+  return output, nil
 }
 
 // decrypt some data using the given password
-func decrypt(data []byte, password string) []byte {
+func decrypt(data []byte, password string) ([]byte, error) {
   // make sure our data is of the minimum length, at least
   if (len(data) < SaltSize + aes.BlockSize) {
-    panic("data too short")
+    return nil, errors.New("data too short")
   }
 
   // read the salt, IV, and ciphertext from our blob
@@ -102,14 +96,14 @@ func decrypt(data []byte, password string) []byte {
 
   // decrypt the ciphertext
   block, err := aes.NewCipher(key)
-  if err != nil { panic(err) }
+  if err != nil { return nil, err }
 
   plaintext := make([]byte, len(ciphertext))
   stream := cipher.NewCFBDecrypter(block, iv)
   stream.XORKeyStream(plaintext, ciphertext)
 
   // return the decrypted ciphertext
-  return plaintext
+  return plaintext, nil
 }
 
 // given a password string, return the hashed variant
@@ -123,35 +117,46 @@ func hashPassword(password string, salt []byte) []byte {
 }
 
 // load raw JSON from some database file bytes and a password
-func load(data []byte, password string) map[string]interface{} {
-  return loadJSON(decompress(decrypt(data, password)))
+func load(data []byte, password string) (*js.Json, error) {
+  compressed, err := decrypt(data, password)
+  if (err != nil) { return nil, err }
+
+  plaintext, err := decompress(compressed)
+  if (err != nil) { return nil, err }
+
+  return js.NewJson(plaintext)
 }
 
 // given JSON, encrypt it to our database format using a password
-func dump(data *map[string]interface{}, password string) []byte {
-  return encrypt(compress(dumpJSON(data)), password)
+func dump(data *js.Json, password string) ([]byte, error) {
+  json, err := data.Encode()
+  if (err != nil) { return nil, err }
+
+  compressed, err := compress(json)
+  if (err != nil) { return nil, err }
+
+  return encrypt(compressed, password)
 }
 
 func main() {
   // some test database data
-  data := make(map[string]interface{})
-  data["hello"] = "world"
-  data["foo"] = 1
-  data["bar"] = 2.25
-  data["baz"] = true
-  data["pants"] = map[string]interface{} {
-    "something_else": false,
-    "bad": nil,
-  }
+  data := js.New()
+  data.Set("hello", "world")
+  data.Set("foo", 1)
+  data.Set("bar", 2.25)
+  data.Set("baz", true)
+  data.Set("pants", js.New());
+  data.Get("pants").Set("something_else", false)
+  data.Get("pants").Set("bad", nil)
 
   password := "password123"
   plaintext := []byte("hello, world!")
   fmt.Printf("plaintext:\t%s\n", plaintext)
 
-  ciphertext := encrypt(plaintext, password)
+  ciphertext, _ := encrypt(plaintext, password)
   fmt.Printf("ciphertext:\t%#v\n", ciphertext)
 
-  deciphertext := decrypt(ciphertext, password)
+  deciphertext, _ := decrypt(ciphertext, password)
   fmt.Printf("deciphertext:\t%#v\n", deciphertext)
   fmt.Printf("new plaintext:\t%s\n", deciphertext)
 }
