74444f430195ed53430a233d14062b10e343ce30
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2015-01-08 07:25:38 -0600

    Split benchmarks into a separate file

--- crypt_test.go
+++ crypt_test.go
@@ -25,9 +25,7 @@ var AllData [][]byte = [][]byte{
 	UnicodeData,
 }
 
-// a global result for deoptimization and a bunch of random bytes
-var deoptimizer interface{}
-var randomBytes = make([]byte, 512)
+// a bunch of random bytes for whatever
 var _, _ = rand.Read(randomBytes)
 
 // fill some arbitrary bits with random data
@@ -427,75 +425,5 @@ func TestDecryptEmptyPassword(t *testing.T) {
 		decrypted, err := Decrypt(encrypted, password)
 		assert.NoError(t, err)
 		assert.Equal(t, plaintext, decrypted)
-	}
-}
-
-// BENCHMARKS
-//
-// NOTE: these tests all store a result globally to prevent the compiler from
-// optimizing the benchmark function call away since its result isn't being
-// used.
-
-// benchmark password hashing using the defaults used internally
-func BenchmarkHashPassword(b *testing.B) {
-	hashPasswordBenchmarkPassword := randomBytes[32:64]
-	var hashPasswordBenchmarkSalt salt128
-	copy(hashPasswordBenchmarkSalt[:], randomBytes[:32])
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		deoptimizer, _ = hashScrypt(
-			hashPasswordBenchmarkPassword,
-			hashPasswordBenchmarkSalt,
-			1<<16,
-			16,
-			2,
-			128,
-		)
-	}
-}
-
-func BenchmarkCompress(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		deoptimizer, _ = compressGzip(randomBytes)
-	}
-}
-
-func BenchmarkDecompress(b *testing.B) {
-	decompressBenchmarkData, _ := compressGzip(randomBytes)
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		deoptimizer, _ = decompressGzip(decompressBenchmarkData)
-	}
-}
-
-func BenchmarkSign(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		deoptimizer, _ = signSHA512(randomBytes, hmacKey)
-	}
-}
-
-func BenchmarkVerify(b *testing.B) {
-	benchmarkDataSignature, _ := signSHA512(randomBytes, hmacKey)
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		deoptimizer = verifySHA512(randomBytes, hmacKey, benchmarkDataSignature)
-	}
-}
-
-func BenchmarkEncrypt(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		deoptimizer, _ = Encrypt(randomBytes, "password")
-	}
-}
-
-func BenchmarkDecrypt(b *testing.B) {
-	encryptedData, _ := Encrypt(randomBytes, "password")
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		deoptimizer, _ = Decrypt(encryptedData, "password")
 	}
 }
