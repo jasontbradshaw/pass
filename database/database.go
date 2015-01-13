77cf236c3b1251390fccdbf405c05e23762faffc
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2015-01-12 18:21:48 -0600

    Begin stubbing out database structs

--- database/database.go
+++ database/database.go
@@ -1,6 +1,9 @@
 package database
 
-import ()
+import (
+	"net/url"
+	"time"
+)
 
 // IMPLEMENTATION NOTES:
 // - encrypt PII separately from the main database
@@ -22,61 +25,71 @@ import ()
 //   generate several passwords at once.
 // - include history for all modifications
 
-// the database looks like this:
-/*
-{
-  // the current version of the database format, as an integer. this is
-  // incremented every time the format changes, so that migrations may be done.
-  "Version": 0,
+type databaseVersion int32
+type RecordId [16]byte
 
-  "Settings": {
-    "ClipboardClearDelayMS": 10000,
-    "DatabaseLockDelayMS": 30000,
-  },
+// all the configurable settings allowed in our database
+type DatabaseSettings struct {
+	// how long to keep the copied data in the clipboard before overwriting it, in
+	// milliseconds.
+	// NOTE: make sure to compare the data in the clipboard to the
+	// originally-copied data, so we don't blindly overwrite the user's new copied
+	// data!
+	ClipboardClearDelayMS int32
 
-  "Entries": {
-    "uuid0": {...},
-    "uuid1": {...},
-    "uuid2": {...},
-    "uuid3": {...},
-    "uuid4": {...},
-  }
+	// how long to wait before saving and closing the database, in milliseconds
+	DatabaseLockDelayMS int32
+
+	// the maximum number of previous versions to keep. negative means "no limit"
+	// NOTE: the user should have to confirm before lowering this, since a history
+	// trim should take place every time it's changed, and lowering it is
+	// effectively the same as deleting old history.
+	PreviousVerionsToKeep int32
 }
-*/
 
-// decrypted entries look like this:
-/*
-{
-  // a UUID for this entry
-  "Id": "00000000-0000-0000-0000-000000000000",
+type Database struct {
+	// incremented every time the format changes so that migrations may be done
+	Version databaseVersion
 
-  // ISO-8601 timestamps for each value
-  "CreatedAt": "2014-12-20T14:42:30Z",
-  "UpdatedAt": "2014-12-20T14:42:30Z",
-  "DeletedAt": nil,
+	Settings DatabaseSettings
 
-  // standard data
-  "Title": "Hacker News",
-  "URL": "https://news.ycombinator.com",
-  "Username": "pg",
-  "Password": "sjf3489yrhlOFasdfklj44445",
-
-  // an array of GMail-like string tags, in sorted order, de-duplicated
-  "Tags": []
-
-  // a map of arbitrary string key/value pairs the user can create and manage
-  "Data": {},
-
-  // a wholesale copy of the previous version of the entry, made every time a
-  // modification is saved.
-  "PreviousVersion": {}
+	// a map of record id to record
+	Records map[RecordId]*Record
 }
-*/
+
+type Record struct {
+	// a unique id for this entry
+	Id RecordId
+
+	// timestamps for each value
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt time.Time
+
+	// standard data
+	Title    string
+	URL      url.URL
+	Username string
+	Password []byte
+
+	// an array of GMail-like string tags, in sorted order, de-duplicated
+	Tags []string
+
+	// a map of arbitrary string key/value pairs the user can create and manage
+	Data map[string]string
+
+	// a pointer to a wholesale copy of the previous version of the entry, made
+	// every time a modification is saved. this will allow for any desired
+	// undo/revert/restore operations to take place, and will be
+	// forward-compatible as long as child entries are migrated to newer database
+	// versions along with their parents.
+	PreviousVersion *Record
+}
 
 // OPERATIONS:
-// - create
-// - query
-// - update
-// - delete
-// - trim_history(count)
-// - trim_deleted(age)
+// - Create
+// - FindBy*
+// - Update
+// - Delete
+// - TrimHistory(count)
+// - TrimDeleted(age)
